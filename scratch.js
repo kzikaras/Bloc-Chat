e26333090007c9d10cca149ae9b1e6eb97700f5f
Kurt Zikaras <kurtzikaras@gmail.com>
2018-01-28 13:21:51 -0500

    Heroku Deploy

---
+++ scratch.js
@@ -0,0 +1,8 @@
+// "typescript": "~2.3.3",
+    // "@angular/core": "1.6.4", 
+    // "@angular/cli": "^1.6.6",
+    // "@angular/compiler-cli": "^5.2.2",
+    // "express": "^4.16.2",
+    // "hapi": "~16.4.0",
+    // "inert": "^4.2.0",
+    // "path": "^0.4.9"
